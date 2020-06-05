0109c672bbe336d7518656fa687ba14cb570990a
chylex <contact@chylex.com>
2020-06-05 03:55:22 +0200

    Work around broken smooth scrolling in notifications

--- Resources/Scripts/notification.js
+++ Resources/Scripts/notification.js
@@ -100,6 +100,43 @@
   })();
   
   //
+  // Block: Work around broken smooth scrolling.
+  //
+  (function(){
+    const doc = document.documentElement;
+    let targetY = 0;
+    let delay = -1;
+    let scrolling = false;
+    
+    window.TDGF_scrollSmoothly = function(delta){
+      targetY += delta;
+      
+      if (targetY < 0){
+        targetY = 0;
+      }
+      else if (targetY > doc.scrollHeight){
+        targetY = doc.scrollHeight;
+      }
+      
+      const prevY = window.scrollY;
+      window.scrollTo({ top: targetY, left: window.scrollX, behavior: "smooth" });
+      scrolling = true;
+      
+      const diff = Math.abs(targetY - prevY);
+      const time = 420 * (Math.log(diff + 510) - 6);
+      
+      clearTimeout(delay);
+      delay = setTimeout(function(){ scrolling = false; }, time);
+    };
+    
+    window.addEventListener("scroll", function(){
+      if (!scrolling){
+        targetY = window.scrollY;
+      }
+    });
+  })();
+  
+  //
   // Block: Work around clipboard HTML formatting.
   //
   document.addEventListener("copy", function(e){
